210a85bf18a2a41d5942128d2d472b2341818142
Neil McLaughlin <neil@Neils-MacBook-Pro.local>
2013-06-21 00:52:22 +0100

    tweaking of help positioning and display

--- public/javascripts/park-bench-panel-init.js
+++ public/javascripts/park-bench-panel-init.js
@@ -6,7 +6,16 @@ $(document).ready(function() {
       autoOpen: false,
       height: 430,
       width: 290,
-      modal: true
+      modal: true,
+      position: { my: "top", at: "bottom", of: btnHelp },
+      show: {
+        effect: "blind",
+        duration: 750
+      },
+      hide: {
+        effect: "blind",
+        duration: 750
+      }
     });
   $( document ).tooltip();
 
